28e3fb1dffd6d51a4c9a3cb4467e15c7bac71eed
JastinXyz <jastinlinggar@gmail.com>
2023-08-31 13:55:45 +0700

    using mysqldump library instead

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require("path");
 const fs = require("fs");
 
 const mysql = require("mysql");
+const mysqldump = require('mysqldump');
 const cron = require("node-cron");
 const { exec } = require("child_process");
 const { config, databases } = require("./config.js");
@@ -13,12 +14,20 @@ const utc = require('dayjs/plugin/utc');
 const timezone = require('dayjs/plugin/timezone');
 
 dayjs.locale(config.dayjs.locale);
-dayjs.extend(utc)
-dayjs.extend(timezone)
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.tz.setDefault(config.dayjs.timezone);
 
 const backupDatabase = (database) => {
-  const currentDate = dayjs().tz(config.dayjs.timezone);
-  const formattedDate = currentDate.format(config.dayjs.format);
+  const connectionConfig = {
+    host: database.host || "localhost",
+    user: database.user,
+    password: database.password,
+    database: database.name,
+    port: database.port || 3306
+  }
+
+  const formattedDate = dayjs().format(config.dayjs.format);
   const backupFilename = `${database.name} ${formattedDate}.sql`;
   const backupPath = path.join(__dirname, "database", database.name);
 
@@ -26,13 +35,7 @@ const backupDatabase = (database) => {
     fs.mkdirSync(backupPath, { recursive: true });
   }
 
-  const connection = mysql.createConnection({
-    host: database.host || "localhost",
-    user: database.user,
-    password: database.password,
-    database: database.name,
-    port: database.port || 3306
-  });
+  const connection = mysql.createConnection(connectionConfig);
 
   connection.connect((error) => {
     if (error) {
@@ -40,7 +43,7 @@ const backupDatabase = (database) => {
       return;
     }
 
-    connection.query("SHOW DATABASES", (error, results) => {
+    connection.query("SHOW DATABASES", async (error, results) => {
       if (error) {
         console.error(`Error querying databases for ${database.name}: ${error}`);
         connection.destroy();
@@ -49,18 +52,17 @@ const backupDatabase = (database) => {
 
       const databaseNames = results.map((result) => result.Database);
       if (databaseNames.includes(database.name)) {
-        const backupCommand = `mysqldump -u ${database.user} -p="${database.password}" --databases ${database.name} > "${backupPath}/${backupFilename}"`;
-
-        // Perform backup command
-        exec(backupCommand, (error, stdout, stderr) => {
-          if (error) {
-            console.error(`Error performing backup for ${database.name}: ${error}`);
-          } else {
-            console.log(`Backup completed for ${database.name}:`, backupFilename);
-          }
-
+        try {
+          await mysqldump({
+            connection: connectionConfig,
+            dumpToFile: `${backupPath}/${backupFilename}`,
+          });
+  
           connection.end();
-        });
+        } catch (err) {
+          console.error(err);
+          connection.end();
+        }
       } else {
         console.error(`Database ${database.name} not found.`);
         connection.end();

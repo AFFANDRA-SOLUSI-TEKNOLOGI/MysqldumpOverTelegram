468ad21bf1b54fd50b1870a1669a3204fbd6ece7
JastinXyz <jastinlinggar@gmail.com>
2023-09-02 20:42:33 +0700

    user id now using env

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ const config: Config = {
   telegram: {
     bot_token: process.env.TG_BOT_TOKEN || "",
     chat_id: process.env.TG_CHAT_ID || "",
-    whitelisted_user_id: [],
+    whitelisted_user_id: process.env.TG_WHITELISTED_USER_ID?.split(", ") as string[],
   },
 
   dayjs: {

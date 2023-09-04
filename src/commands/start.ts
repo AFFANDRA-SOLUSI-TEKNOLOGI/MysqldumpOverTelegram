ca8b3f65616a869abf2d609416e88999f5d33d5d
JastinXyz <jastinlinggar@gmail.com>
2023-09-04 16:56:37 +0700

    impruv start commands

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,8 +1,18 @@
-import { Context } from "telegraf";
+import { Context, Markup } from "telegraf";
+import { CommandHandler } from "../server";
 
 module.exports = {
     name: "start",
+    aliases: ["help", "menu"],
     async execute(ctx: Context) {
-        ctx.reply("Hello World!");
+        let all = Array.from(CommandHandler.commands.values());
+        let commands: string[] = [];
+
+        all.map((x: { name: string, execute: Promise<any> }) => commands.push(x.name));
+        ctx.reply(`<b>Welcome @${ctx.from?.username}!</b>\n\n<i>🤖 Command List:\n/${commands.join(", /")}</i>`, {
+            parse_mode: 'HTML', ...Markup.inlineKeyboard([
+                Markup.button.url('🐈 Source Code on Github', 'https://github.com/AFFANDRA-SOLUSI-TEKNOLOGI/MysqldumpOverTelegram')
+            ])
+        });
     }
 }
--- src/server.ts
+++ src/server.ts
@@ -153,4 +153,4 @@ bot.launch();
 process.once("SIGINT", () => bot.stop("SIGINT"));
 process.once("SIGTERM", () => bot.stop("SIGTERM"));
 
-export { db };
+export { db, CommandHandler };

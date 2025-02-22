4fddc6d63b528ed23e03ad30f3490adaf7837f20
JastinXyz <jastinlinggar@gmail.com>
2025-02-22 15:35:48 +0700

    fix: typo

--- src/commands/delete.ts
+++ src/commands/delete.ts
@@ -7,7 +7,7 @@ module.exports = {
   async execute(ctx: Context) {
       let get = await db.get("databases");
 
-      if (!get || !get.length) return ctx.reply("You dont have any website to delete!");
+      if (!get || !get.length) return ctx.reply("You dont have any database to delete!");
       let button: any = [];
     
       get.map((x: any) => button.push(Markup.button.callback(x.name, `delete ${x.name}`)));
--- src/commands/dump.ts
+++ src/commands/dump.ts
@@ -7,7 +7,7 @@ module.exports = {
   async execute(ctx: Context) {
       let get = await db.get("databases");
 
-      if (!get || !get.length) return ctx.reply("You dont have any website to dump!");
+      if (!get || !get.length) return ctx.reply("You dont have any database to dump!");
       let button: any = [];
     
       get.map((x: any) => button.push(Markup.button.callback(x.name, `backup ${x.name}`)));
